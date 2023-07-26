da4d982564f570b9a3513d2005cb2649f58fb414
Andrew Sykes <andrewsykes@btinternet.com>
2023-07-26 16:51:01 +0100

    partials

--- app.mjs
+++ app.mjs
@@ -1,68 +1,30 @@
-/*var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-*/
-
 import { default as express } from 'express';
-import { default as hbs } from'hbs';
+import hbs from 'hbs';
 import * as path from 'path';
 // import * as favicon from 'serve-favicon';
 import { default as logger } from 'morgan';
 import { default as cookieParser } from 'cookie-parser';
 import { default as bodyParser } from 'body-parser';
 import * as http from 'http';
-import { approotdir } from './approotdir.mjs';
+import approotdir  from './approotdir.mjs';
+
+import { InMemoryNotesStore } from './models/notes-memory.mjs';
+export const NotesStore = new InMemoryNotesStore();
+
+
 const __dirname = approotdir;
 import {
 normalizePort, onError, onListening, handle404, basicErrorHandler
 } from './appsupport.mjs';
 import { router as indexRouter } from './routes/index.mjs';
 // import { router as notesRouter } from './routes/notes.mjs';
-/*
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-*/
 
 export const app = express();
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'partials'));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -78,4 +40,10 @@ app.use('/', indexRouter);
 app.use(handle404);
 app.use(basicErrorHandler);
 export const port = normalizePort(process.env.PORT || '3000');
+
 app.set('port', port);
+
+export const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
--- routes/index.mjs
+++ routes/index.mjs
@@ -1,17 +1,18 @@
-/*var express = require('express');
-var router = express.Router();
-
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-*/
-import { default as express } from 'express';
+import * as express from "express";
+import { NotesStore as notes } from "../app.mjs";
 export const router = express.Router();
-
-router.get('/', async (req, res, next) => {
-//... placeholder for Notes home page code
-res.render('index', { title: 'Notes' });
+/* GET home page. */
+router.get("/", async (req, res, next) => {
+  try {
+    const keylist = await notes.keylist();
+    // console.log(`keylist ${util.inspect(keylist)}`);
+    const keyPromises = keylist.map((key) => {
+      return notes.read(key);
+    });
+    const notelist = await Promise.all(keyPromises);
+    // console.log(util.inspect(notelist));
+    res.render("index", { title: "Notes", notelist: notelist });
+  } catch (err) {
+    next(err);
+  }
 });
