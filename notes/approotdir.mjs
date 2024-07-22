8166f32d62735ce1bfc355376227433f1e256a24
Andrew Sykes <andrewsykes@btinternet.com>
2024-07-22 09:31:03 +0100

    tidy

--- notes/approotdir.mjs
+++ notes/approotdir.mjs
@@ -1,5 +1,5 @@
-import * as path from 'path';
-   import * as url from 'url';
-   const __filename = url.fileURLToPath(import.meta.url);
-   const __dirname = path.dirname(__filename);
-   export const approotdir = __dirname;
+import * as path from "path";
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const approotdir = __dirname;
